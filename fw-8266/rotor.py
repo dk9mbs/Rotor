8217a27f9b9c6d9bc609d0b57435218bfcdc31bc
dk9mbs <dk9mbs@heiligenhafen.the-b.eu>
2021-01-01 18:01:54 +0100

    First stable version

--- fw-8266/rotor.py
+++ fw-8266/rotor.py
@@ -26,42 +26,69 @@ def do_connect(essid, password):
             pass
     print('network config:', sta_if.ifconfig())
 
-def run_webserver():
-    html='''<!DOCTYPE html>
-    <html>
-    <head><title>AG5ZL</title></head>
-    <center><h2>WebServer for turning LED on </h2></center>
-    <form>
-    <button name="LED" value='ON' type='submit'> LED ON </button>
-    <button name="LED" value='OFF' type='submit'> LED OFF </button>
-    <br><br>
-    '''
-    s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    s.setblocking(True)
-    s.bind(('',80))
-    s.listen(5)
+class WebRequest:
+    def __init__(self, f):
+        self._header=[]
+        self._method=""
+        self._request_url=""
+        #read the first line with method request url and http version
+        req = str(f.readline().decode()).split(" ")
+        self._request_url=req[1]
+        #read the header until end of header
+        while True:
+            line = f.readline()
+            self._header.append(line)
+            if not line or line == b'\r\n':
+                break
 
-    while True:
-        conn, addr = s.accept()
-        print('Got a connection from %s' % str(addr))
-        request = conn.recv(1024)
-        request = str(request)
-        print('Content = %s' % request)
-        led_on = request.find('/?led=on')
-        led_off = request.find('/?led=off')
-        if led_on == 6:
-            print('LED ON')
-            #led.value(1)
-        if led_off == 6:
-            print('LED OFF')
-            #led.value(0)
-        response = html
-        conn.send('HTTP/1.1 200 OK\n')
-        conn.send('Content-Type: text/html\n')
-        conn.send('Connection: close\n\n')
-        conn.sendall(response)
-        conn.close()
+
+class WebServer:
+    def __init__(self):
+        pass
+
+    def run(self):
+        html='''<!DOCTYPE html>
+        <html>
+        <head><title>AG5ZL</title></head>
+        <center><h2>WebServer for turning LED on </h2></center>
+        <form>
+        <button name="LED" value='ON' type='submit'> LED ON </button>
+        <button name="LED" value='OFF' type='submit'> LED OFF </button>
+        <br><br>
+        '''
+        s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        s.setblocking(True)
+        s.bind(('',80))
+        s.listen(5)
+
+        while True:
+            conn, addr = s.accept()
+            print('Got a connection from %s' % str(addr))
+            #request = conn.recv(1024)
+
+            f=conn.makefile('rwb', 0)
+            request=WebRequest(f)
+
+            print(request._request_url)
+            #read the first line with method request url and http version
+            #req = str(f.readline().decode()).split(" ")
+            #request_url=req[1]
+            #print("Request URL: %s" % request_url)
+            #read the header until end of header
+            #while True:
+            #    line = f.readline()
+            #    if not line or line == b'\r\n':
+            #        break
+
+            # send the response
+
+            response = html
+            conn.send('HTTP/1.1 200 OK\n')
+            conn.send('Content-Type: text/html\n')
+            conn.send('Connection: close\n\n')
+            conn.sendall(response)
+            conn.close()
 
 
 do_connect(cfg['wlan']['essid'], cfg['wlan']['password'])
-run_webserver()
+WebServer().run()
